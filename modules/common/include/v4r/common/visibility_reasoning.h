0f0d9bda8c4a43c98c7e467bc2aaeb2e52cb18c5
STRANDS Jenkins <strands@hanheide.net>
2015-09-16 09:37:53 +0000

    Rebase from 'debian/indigo/v4r'

--- modules/common/include/v4r/common/visibility_reasoning.h
+++ modules/common/include/v4r/common/visibility_reasoning.h
@@ -59,9 +59,9 @@ namespace v4r
           tss_ = t;
         }
 
-        float computeFocalLength(int cx, int cy, const typename pcl::PointCloud<PointT>::ConstPtr & cloud);
+        float computeFocalLength(int width, int height, const typename pcl::PointCloud<PointT>::ConstPtr & cloud);
 
-        void computeRangeImage(int cx, int cy, float fl, const typename pcl::PointCloud<PointT>::ConstPtr & cloud, typename pcl::PointCloud<PointT>::Ptr & range_image);
+        static void computeRangeImage(int width, int height, float fl, const typename pcl::PointCloud<PointT>::ConstPtr & cloud, typename pcl::PointCloud<PointT>::Ptr & range_image);
     };
   }
 }
